14cffd11bece869726bc176d4147162795db132e
Nguyen Tran <merlinvn@gmail.com>
2020-12-09 18:26:33 -0500

    add setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+rm -f ~/.gitconfig
+rm -f ~/.inputrc
+rm -f ~/.zshrc
+rm -f ~/.vimrc
+
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.inputrc ~/.inputrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
