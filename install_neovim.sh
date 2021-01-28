f80040633f836cbae45e6a935b6a798a953ae591
Nguyen Tran <merlinvn@gmail.com>
2021-01-28 00:18:52 -0500

    fix typo install_neovim.sh

--- install_neovim.sh
+++ install_neovim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-commoni
+sudo apt-get install software-properties-common
 sudo apt-get install python-dev python-pip python3-dev python3-pip
 
 sudo add-apt-repository ppa:neovim-ppa/unstable
