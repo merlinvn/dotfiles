1e49ad6c0445ed7df34f70234144d5da16e6c2df
Nguyen Tran <merlinvn@gmail.com>
2021-08-26 14:17:52 -0400

    install alacritty and nerd fonts script

---
+++ install_nerd-fonts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Mononoki Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Mononoki/Regular/complete/mononoki-Regular%20Nerd%20Font%20Complete.ttf 
+
+
+
