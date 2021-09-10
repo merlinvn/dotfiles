bab28d121531a4a72ba2a9daf76a7205b771eacf
Nguyen Tran <merlinvn@gmail.com>
2021-09-10 12:44:55 -0400

    add tmux config

--- nvim/.config/nvim/lua/settings/colors.lua
+++ nvim/.config/nvim/lua/settings/colors.lua
@@ -5,3 +5,11 @@ vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_disable_italic_comment = 1
 vim.cmd 'colorscheme gruvbox-material'
 vim.cmd 'highlight HighlightedyankRegion cterm=reverse gui=reverse'
+
+-- vim.cmd([[
+-- if exists("+termguicolors")
+--   let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+--   let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+--   set termguicolors
+-- endif
+-- ]])
