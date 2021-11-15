89e52f022d0737170d0e7e9cf1ece279bdbfcbf6
Nguyen Tran <merlinvn@gmail.com>
2021-11-14 22:10:14 -0500

    fix download name file of go install

--- install_go.sh
+++ install_go.sh
@@ -2,7 +2,7 @@
 
 #download first
 GO_VERSION=$(curl 'https://golang.org/VERSION?m=text')
-curl -o go${GO_VERSION}.linux-amd64.tar.gz -fSL https://golang.org/dl/${GO_VERSION}.linux-amd64.tar.gz
+curl -o ${GO_VERSION}.linux-amd64.tar.gz -fSL https://golang.org/dl/${GO_VERSION}.linux-amd64.tar.gz
 
 
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${GO_VERSION}.linux-amd64.tar.gz
