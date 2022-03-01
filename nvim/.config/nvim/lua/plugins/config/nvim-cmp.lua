2a2a51cba478d5f4a12bc5be24de3f745099a98b
Nguyen Tran <merlinvn@gmail.com>
2022-03-01 00:09:48 -0500

    update completion key

--- nvim/.config/nvim/lua/plugins/config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-cmp.lua
@@ -36,16 +36,15 @@ function M.setup()
       end
     },
     mapping = {
-      ["<C-h>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-,>"] = cmp.mapping.scroll_docs(4),
-      ["<C-o>"] = cmp.mapping.close(),
-      ["<C-y>"] = cmp.mapping.confirm(
-        {
-          behavior = cmp.ConfirmBehavior.Insert,
-          select = true
-        }
-      ),
-      ["<C-Space>"] = cmp.mapping.complete()
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
       -- ["<C-Space>"] = cmp.mapping(
       --   function(fallback)
