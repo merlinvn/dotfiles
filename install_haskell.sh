1467bf9e4cd273ca584df29e3ab9a2f3fbfd2cd1
Nguyen Tran <merlinvn@gmail.com>
2022-05-01 20:30:01 -0400

    update haskell install script

--- install_haskell.sh
+++ install_haskell.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# install ghcup
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install  build-essential curl libffi-dev libffi7 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5
+
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+
 # this script is used to install / upgrade stack for haskell in raspberry pi only
 # will take a very long time to finish
 
-sudo apt update && sudo apt upgrade -y
-
-sudo apt install build-essential libnuma1 libnuma-dev libtinfo-dev libtinfo5 libtinfo6 libc6-dev
-
-sudo apt install llvm
-
-sudo apt install clang ninja-build
-
-sudo apt install haskell-stack
-
-stack update
-
-stack upgrade
-
+# sudo apt install llvm
+#
+# sudo apt install clang ninja-build
+#
+# sudo apt install haskell-stack
+#
+# stack update
+#
+# stack upgrade
+#
