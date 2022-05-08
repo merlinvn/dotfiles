dabfc211a96654624fd1874fe0e1c9bff95ff863
Nguyen Tran <merlinvn@gmail.com>
2022-05-08 17:20:59 -0400

    neovim update config to work in Windows

--- nvim/.config/nvim/lua/merlinvn/telescope/init.lua
+++ nvim/.config/nvim/lua/merlinvn/telescope/init.lua
@@ -1,12 +1,27 @@
 local M = {}
+
+local dotfiles_path = "$HOME/dotfiles/"
+
+if vim.fn.has('win32') == 1 then
+  dotfiles_path = "~/.config"
+end
+
 M.search_dotfiles = function()
   require("telescope.builtin").find_files(
     {
     prompt_title = "< My dotfiles >",
-    cwd = "$HOME/dotfiles/",
+    cwd = dotfiles_path,
     find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" }
   }
   )
+end
+
+M.project_files = function()
+  local opts = {
+    find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" }
+  } -- define here if you want to define something
+  local ok = pcall(require "telescope.builtin".git_files, opts)
+  if not ok then require "telescope.builtin".find_files(opts) end
 end
 
 return M
--- nvim/.config/nvim/lua/merlinvn/telescope/setup.lua
+++ nvim/.config/nvim/lua/merlinvn/telescope/setup.lua
@@ -8,9 +8,24 @@ require("telescope").setup {
     file_previewer = require("telescope.previewers").vim_buffer_cat.new,
     qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
     -- mappings = {i = {["<C-x>"] = false, ["<C-q>"] = actions.send_to_qflist}},
-    mappings = {i = {["<C-q>"] = actions.send_to_qflist}},
-    set_env = {["COLORTERM"] = "truecolor"},
-    file_ignore_patterns = {"ext/.*"} -- comment this line for non C++ project
+    mappings = { i = { ["<C-q>"] = actions.send_to_qflist } },
+    set_env = { ["COLORTERM"] = "truecolor" },
+    -- file_ignore_patterns = { "ext/.*" } -- comment this line for non C++ project
+  },
+  pickers = {
+    find_files = {
+      mappings = {
+        n = {
+          ["cd"] = function(prompt_bufnr)
+            local selection = require("telescope.actions.state").get_selected_entry()
+            local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+            require("telescope.actions").close(prompt_bufnr)
+            -- Depending on what you want put `cd`, `lcd`, `tcd`
+            vim.cmd(string.format("silent lcd %s", dir))
+          end
+        }
+      }
+    },
   },
   extensions = {
     fzf = {
