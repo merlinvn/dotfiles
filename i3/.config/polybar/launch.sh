55e97b68332609a1a1cc865cecc1dadaf9006a8a
Nguyen Tran <merlinvn@gmail.com>
2022-12-02 14:14:36 -0500

    polybar add wifi icon

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do : ; done
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload bar &
+    WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload bar &
   done
 else
   polybar --reload bar &
