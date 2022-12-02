3c2ed4909b456c0681f2eb2c2a70bdf8dbd79225
Nguyen Tran <merlinvn@gmail.com>
2022-12-02 15:23:31 -0500

    update polybar launch, i3 config

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -3,9 +3,9 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
+# polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do : ; done
