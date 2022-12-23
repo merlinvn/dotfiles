c12e8525695498b180f46aa11a596e8fd1cef15d
Nguyen Tran <merlinvn@gmail.com>
2022-12-23 10:45:46 -0500

    update neovim keymap

--- nvim/.config/nvim/after/plugin/keymap/init.lua
+++ nvim/.config/nvim/after/plugin/keymap/init.lua
@@ -9,6 +9,8 @@
 vim.keymap.set("n", "<C-s>", ":w<CR>", { silent = true })
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { silent = true })
 
+vim.keymap.set("n", "<Leader>x", vim.cmd.Ex)
+
 -- remap quit
 vim.keymap.set("n", "<Leader>q", ":bd<CR>", { silent = true, desc = "Close current buffer" })
 vim.keymap.set("n", "<C-q>", ":q<CR>", { silent = true })
@@ -58,36 +60,20 @@ vim.keymap.set("n", "<leader>6", "6gt", { silent = true, remap = true })
 vim.keymap.set("n", "<leader>7", "7gt", { silent = true, remap = true })
 vim.keymap.set("n", "<leader>8", "8gt", { silent = true, remap = true })
 vim.keymap.set("n", "<leader>9", "9gt", { silent = true, remap = true })
-vim.keymap.set("n", "<leader>0", ":tablast<cr>", { silent = true, remap = true } )
+vim.keymap.set("n", "<leader>0", ":tablast<cr>", { silent = true, remap = true })
 
 -- create new tab
 vim.keymap.set("n", "<C-t>", "<C-w>s<C-w>T")
 -- x for close
 vim.keymap.set("n", "<C-x>", ":tabclose<CR>")
 
--- vim.keymap.set("n", "<C-l>", ":tabnext<cr>")
--- vim.keymap.set("n", "<C-h>", ":tabprevious<cr>")
-
---move tab lnnt right
--- vim.keymap.set("n", "<C-S-h>", ":tabm-<cr>")
--- vim.keymap.set("n", "<C-S-l>", ":tabm+<cr>")
--- vim.keymap.set("n", "<C-S-left>", ":tabm-<cr>")
--- vim.keymap.set("n", "<C-S-right>", ":tabm+<cr>")
-
 -- move line up / down
-vim.keymap.set("n", "<C-j>", ":m .+2<CR>==", { silent = true })
+vim.keymap.set("n", "<C-j>", ":m .+1<CR>==", { silent = true })
 vim.keymap.set("n", "<C-k>", ":m .-2<CR>==", { silent = true })
 vim.keymap.set("i", "<C-j>", "<Esc>:m .+1<CR>==gi", { silent = true })
 vim.keymap.set("i", "<C-k>", "<Esc>:m .-2<CR>==gi", { silent = true })
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { silent = true })
-
--- vim.keymap.set("n", "<C-down>", ":m .+1<CR>==")
--- vim.keymap.set("n", "<C-up>", ":m .-2<CR>==")
--- vim.keymap.set("i", "<C-down>", "<Esc>:m .+1<CR>==gi")
--- vim.keymap.set("i", "<C-up>", "<Esc>:m .-2<CR>==gi")
--- vim.keymap.set("v", "<C-down>", ":m '>+1<CR>gv=gv")
--- vim.keymap.set("v", "<C-up>", ":m '<-2<CR>gv=gv")
 
 -- Go to next / previous, center with zz, and zv to open fold
 vim.keymap.set("n", "n", "nzzzv")
@@ -111,20 +97,35 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
 
-
 -- better p
 -- vim.keymap.set("n", ",p", '"0p')
 -- vim.keymap.set("n", ",P", '"0p')
 vim.keymap.set("v", "p", '"_dP')
 
 -- paste from clipboard
-vim.keymap.set("n", "<C-p>", '"*p')
-vim.keymap.set("i", "<C-p>", '<esc>"*pa')
+vim.keymap.set("n", "<C-p>", '"+p')
+vim.keymap.set("i", "<C-p>", '<esc>"+pa')
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : yank to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- delete to register _ (black hole)
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
 -- vim.keymap.set("n",  "<C-f>", '"*P' )
 
 -- replace selected in visual mode, by yank current selection to register h, then
 -- <C-r>h to paste in command mode, calling <left> * 3 to go back to proper position
 vim.keymap.set("v", "<C-r>", '"hy:%s/<C-r>h//gc<left><left><left>')
+vim.keymap.set("n", "<C-r>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 if vim.fn.has('win32') then
   vim.keymap.set({ "n", "i", "v", "o" }, "<C-z>", "<nop>")
