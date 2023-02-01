5c401dab572b241c3b88ff96215b82fc2a3f672a
Nguyen Tran <merlinvn@gmail.com>
2023-02-01 01:48:00 -0500

    nvim: update which key

--- nvim/.config/nvim/after/plugin/which-key.lua
+++ nvim/.config/nvim/after/plugin/which-key.lua
@@ -2,6 +2,11 @@ require("which-key").setup {
   -- your configuration comes here
   -- or leave it empty to use the default settings
   -- refer to the configuration section below
+  plugins = {
+    presets = {
+      text_objects = false ,
+    }
+  }
 }
 local wk = require("which-key")
 
