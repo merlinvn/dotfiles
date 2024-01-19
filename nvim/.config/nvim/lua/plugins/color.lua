637bd8747ca16a598c4b1c7d5eceefccdc2aaf72
Nguyen Tran <merlinvn@gmail.com>
2024-01-19 16:22:13 -0500

    update nvim color

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -1,8 +1,8 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "default",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "default",
+  --   },
+  -- },
 }
