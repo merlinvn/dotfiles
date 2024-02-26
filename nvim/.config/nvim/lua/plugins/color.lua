abc2bce9e69f0cf86b3bdf14ea88fb631a8438dc
Nguyen Tran <merlinvn@gmail.com>
2024-02-26 01:03:33 -0500

    nvim: clean up

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -11,12 +11,42 @@ return {
       },
     },
   },
-  --
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
+    },
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = { "bold", "italic" },
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+    },
+  },
+
   -- -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
   -- {
--- nvim/.config/nvim/old/init.lua
+++
@@ -1,6 +0,0 @@
-require("core.lazy")
-require("core.options")
-require("core.keymaps")
-require("core.autocmds")
-
--- TODO: still have some bugs with noice and notify which put cpu to 100%
