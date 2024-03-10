31765a8187779db1146a244a7b7adeacc0fd1cc8
Nguyen Tran <merlinvn@gmail.com>
2024-03-10 17:40:14 -0400

    update tmux vim windows navigation

--- nvim/.config/nvim/lua/plugins/smart-splits.lua
+++ nvim/.config/nvim/lua/plugins/smart-splits.lua
@@ -20,23 +20,23 @@ return {
       { "<C-Down>", "<cmd>lua require('smart-splits').resize_down()<cr>", desc = "Resize down" },
       { "<C-Up>", "<cmd>lua require('smart-splits').resize_up()<cr>", desc = "Resize up" },
       { "<C-Right>", "<cmd>lua require('smart-splits').resize_right()<cr>", desc = "Resize right" },
-      {
-        "<C-h>",
-        "<cmd>lua require('smart-splits').move_cursor_left()<cr>",
-        desc = "Move curssor left",
-      },
-      {
-        "<C-j>",
-        "<cmd>lua require('smart-splits').move_cursor_down()<cr>",
-        desc = "Move curssor down",
-      },
-      { "<C-k>", "<cmd>lua require('smart-splits').move_cursor_up()<cr>", desc = "Move cursor up" },
-      {
-        mode = "n",
-        "<C-l>",
-        "<cmd>lua require('smart-splits').move_cursor_right()<cr>",
-        desc = "move cursor right",
-      },
+      -- {
+      --   "<C-h>",
+      --   "<cmd>lua require('smart-splits').move_cursor_left()<cr>",
+      --   desc = "Move curssor left",
+      -- },
+      -- {
+      --   "<C-j>",
+      --   "<cmd>lua require('smart-splits').move_cursor_down()<cr>",
+      --   desc = "Move curssor down",
+      -- },
+      -- { "<C-k>", "<cmd>lua require('smart-splits').move_cursor_up()<cr>", desc = "Move cursor up" },
+      -- {
+      --   mode = "n",
+      --   "<C-l>",
+      --   "<cmd>lua require('smart-splits').move_cursor_right()<cr>",
+      --   desc = "move cursor right",
+      -- },
     },
   },
 }
