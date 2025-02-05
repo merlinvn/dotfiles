48772126d04a36b7ee506a087c472ad272ab0f53
Nguyen Tran <merlinvn@gmail.com>
2025-02-05 06:18:34 +0000

    update nvim clipboard

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -21,3 +21,24 @@ vim.cmd("cnoreabbrev WQ wq")
 vim.cmd("cnoreabbrev Set set")
 vim.cmd("cnoreabbrev SEt set")
 vim.cmd("cnoreabbrev SET set")
+
+-- vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
